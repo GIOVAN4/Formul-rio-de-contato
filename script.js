3c133ac8883366d24d9af6b84441d8f8d957570f
GIOVAN4 <125381987+GIOVAN4@users.noreply.github.com>
2023-08-19 22:10:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function Submit(){
+  const nome = document.getElementById("nome").value;
+  if(nome === ""){
+    alert("ERRADO");
+  }else{
+    alert("CERTO");
+  }
+}
 
+function Submit(){
+  const email = document.getElementById("email").value;
+  if(email === ""){
+    alert("ERRADO");
+  }else{
+    alert("CERTO");
+  }
+}
